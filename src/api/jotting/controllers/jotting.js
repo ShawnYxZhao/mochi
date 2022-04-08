a7f026be4c2b4eab19c6b2771598f083ab346335
Shawn Zhao <97186212+ShawnYxZhao@users.noreply.github.com>
2022-04-08 14:56:03 -0400

    Add jotting

---
+++ src/api/jotting/controllers/jotting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  jotting controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::jotting.jotting');
---
+++ src/api/jotting/routes/jotting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * jotting router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::jotting.jotting');
